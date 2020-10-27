0bf59fec7bcf4d3d5aba7c6a61c5199f216d9715
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-10-26 23:45:50 -0300

    fix wasting energy await error

--- discordjs/wastingEnergy.js
+++ discordjs/wastingEnergy.js
@@ -1,16 +1,17 @@
 const Rooster = require("../models/Rooster");
 
-module.exports = (userId, name, value) => {
-  
-
-  try {
-    const rooster = await Rooster.updateOne({
-      discordId: userId,
-      name: name,
-    }, {
-      $inc: { stamina: -value }
-    });
-  } catch (ex) {
-    console.log("try error wastingEnergy: ", ex);
-  }
+module.exports = async (userId, name, value) => {
+	try {
+		await Rooster.updateOne(
+			{
+				discordId: userId,
+				name: name,
+			},
+			{
+				$inc: { stamina: -value },
+			}
+		);
+	} catch (ex) {
+		console.log("try error wastingEnergy: ", ex);
+	}
 };
