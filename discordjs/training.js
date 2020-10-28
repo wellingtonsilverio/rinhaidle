f6d0eb4fd370dba5aacf2de98deb907675ed2c56
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-10-27 21:30:18 -0300

    remove debbug

--- discordjs/training.js
+++ discordjs/training.js
@@ -9,10 +9,7 @@ module.exports = async (message, name, type) => {
 	incQuery[trainingName[type]] = type === 1 ? 10 : 100;
 
 	try {
-		console.log("userId", userId);
-		console.log("name", name);
 		const stamina = await require("./get-stamina")(userId, name);
-		console.log("stamina", stamina);
 
 		if (stamina < 20) {
 			message.reply("Seu Galo está cansado, espere um pouco para treina-lo");
