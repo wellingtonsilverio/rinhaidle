e6bd43e5562c5f52a5803e606f823799d414e5e8
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-11-21 00:17:54 -0300

    add async

--- discordjs/ranking.js
+++ discordjs/ranking.js
@@ -1,27 +1,30 @@
 const Discordjs = require("discord.js");
 const Rooster = require("../models/Rooster");
 
-module.exports = (message) => {
-  try { 
-    const embed = new Discordjs.MessageEmbed()
-    .setTitle("Ranking")
-    .setColor([203, 153, 126]);
+module.exports = async (message) => {
+	try {
+		const embed = new Discordjs.MessageEmbed()
+			.setTitle("Ranking")
+			.setColor([203, 153, 126]);
 
-    const roosters = await Rooster.find({
-      victoryPoints: { $gt: 0 },
-    }).lean();
+		const roosters = await Rooster.find({
+			victoryPoints: { $gt: 0 },
+		}).lean();
 
-    roosters.sort((a, b) => {
-      return a.victoryPoints - b.victoryPoints;
-    }).slice(0, 10).map((rooster, index) => {
-      embed.addField(
-        `#${index + 1}`,
-        `${rooster.name} de <@!${rooster.discordId}>`
-      );
-    })
+		roosters
+			.sort((a, b) => {
+				return a.victoryPoints - b.victoryPoints;
+			})
+			.slice(0, 10)
+			.map((rooster, index) => {
+				embed.addField(
+					`#${index + 1}`,
+					`${rooster.name} de <@!${rooster.discordId}>`
+				);
+			});
 
-    message.reply(embed);
-  } catch (ex) {
-    console.log("try error recharge: ", ex);
-  }
+		message.reply(embed);
+	} catch (ex) {
+		console.log("try error recharge: ", ex);
+	}
 };
